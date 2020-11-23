fe3a7526517a764469550979de59927279d256c2
Montificus11 <brendan.monte11@gmail.com>
2020-11-23 11:09:47 -0500

    New API works for mapping over search results, now to make the Single page

--- client/components/search.js
+++ client/components/search.js
@@ -1,7 +1,17 @@
 import React from 'react'
 import axios from 'axios'
 
-let movie
+const options = {
+  method: 'GET',
+  url:
+    'https://ivaee-internet-video-archive-entertainment-v1.p.rapidapi.com/entertainment/search/',
+  headers: {
+    'content-type': 'application/json',
+    'x-rapidapi-key': 'fd7b15974cmsh8a07e9234699e3cp16b971jsnd504ba3dbbbb',
+    'x-rapidapi-host':
+      'ivaee-internet-video-archive-entertainment-v1.p.rapidapi.com'
+  }
+}
 
 export default class Movies extends React.Component {
   constructor(props) {
@@ -18,16 +28,25 @@ export default class Movies extends React.Component {
     this.setState({input: event.target.value})
   }
 
-  onSubmit(event) {
+  async onSubmit(event) {
     event.preventDefault()
-    fetch(`http://www.omdbapi.com/?t=${this.state.input}&apikey=35db4ef`)
-      .then(resp => resp.json())
-      .then(data => this.setState({movie: data, clicked: true}))
+    options.params = {Title: this.state.input, ProgramTypes: 'Movie'}
+    let response = await axios.request(options)
+    try {
+      console.log(response.data, 'response in onSubmit')
+      this.setState({movie: response.data, clicked: true})
+    } catch (error) {
+      console.log('error')
+    }
+    // fetch(`http://www.omdbapi.com/?t=${this.state.input}&apikey=35db4ef`)
+    //   .then(resp => resp.json())
+    //   .then(data => this.setState({movie: data, clicked: true}))
     // console.log(response, 'response in component did mount')
     // this.props.history.push('/movies')
   }
   render() {
     console.log(this.state.movie)
+    const movies = this.state.movie.Hits
     return (
       <div>
         <h1>This is a page where you can search movies!</h1>
@@ -40,15 +59,18 @@ export default class Movies extends React.Component {
           <button type="submit" onClick={this.onSubmit}>
             Search
           </button>
-          {this.state.clicked && (
-            <div className="movie-info">
-              <div>{this.state.movie.Title}</div>
-              <div>{this.state.movie.Director}</div>
-              <div>{this.state.movie.Released}</div>
-              <div>{this.state.movie.Plot}</div>
-              <img src={this.state.movie.Poster} />
-            </div>
-          )}
+          {this.state.clicked &&
+            movies.map(movie => {
+              return (
+                <div className="movie-info" key={movie.id}>
+                  <div>{movie.Source.Title}</div>
+                  {/* <div>{movie.Source.Director}</div> */}
+                  <div>{movie.Source.Year}</div>
+                  {/* <div>{movie.Source.Plot}</div> */}
+                  {/* <img src={movie.Source.Poster} /> */}
+                </div>
+              )
+            })}
         </form>
       </div>
     )
